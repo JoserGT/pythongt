87d4c1558c82fc5259415ff59491b49d79197303
Jose Villeda <Jvilleda@trelec.net>
2020-08-02 09:12:28 -0600

    primer commit conversor quetzales a dolares

--- Conversor.py
+++ Conversor.py
@@ -1,7 +1,7 @@
-pesos = input("Cuantos pesos colombianos tienes:  ")
-pesos = float(pesos)
-valor_dolar = 3875
-dolares = pesos/valor_dolar
+quetzal = input("Cuantos quetzales guatemaltecos tienes:  ")
+quetzal = float(quetzal)
+valor_dolar = 7.70871
+dolares = quetzal/valor_dolar
 dolares = round (dolares,2)
 dolares = str(dolares)
 print("Tienes  $" + dolares  + " dolares")
